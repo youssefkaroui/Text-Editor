626664aa2bb5040b794e3078c1783ae2581ffc9a
Youssef <youssefkaroui6@gmail.comm>
2023-09-30 14:18:25 -0400

    added logic to create data content in the db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,9 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
+export const putDb = async (content) => { 
+  console.error('putDb not implemented');
+  // Creates the db connection
+  const contactDb = await openDB('jate', 1);
+  
+  // Creates a new instance.
+  const tx = contactDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+
+  // awaits the response from db
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
